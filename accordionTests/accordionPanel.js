e2d71fb5e96fceb33bf1422c119cb6e99fb58eae
Sherman Marshall <sherman.david.marshall@gmail.com>
2018-01-12 00:11:54 -0800

    Initial commit. Adding Backbone/Bootstrap accordion view. Still need to work out dependencies and tests with require

---
+++ accordionTests/accordionPanel.js
@@ -0,0 +1,54 @@
+define(function(require) {
+	var Backbone 	= require('backbone');
+	var _		= require('underscore');
+
+	var Item = Backbone.Model.extend({
+		defaults: {
+			link: '',
+			name: ''
+		}
+	});
+	var Group = Backbone.Model.extend({
+		initialize: function() {
+			this.set('items', []);
+		},
+		defaults: {
+			name: '',
+		},
+		addItem: function(params) {
+			this.get('items').push(new Item(params));
+		}
+	});
+	var AccordionCollection = Backbone.Collection.extend({
+		model: Group,
+	});
+	var Panel = Backbone.View.extend({
+		el: '#template-panel-content',
+		initialize: function(object) {
+			this.createTemplate();
+		},
+		createTemplate: function() {
+			var panelTemplate = "<div class='panel panel-default'>";
+			//Iterate over the content to display each individual item
+			this.collection.models.forEach(function(item) {
+				//If the item in the collection is a group, handle all elements
+				if (item.get('items') && item.get('items') instanceof Array) {
+					panelTemplate += "<div class='panel-heading'><h4 class='panel-title'><a data-toggle='collapse' " +
+							"data-target='#" + item.get('name') + "'>" + item.get('name') + "</a></h4></div><div id='" +
+							item.get('name') + "' class='panel-collapse collapse'>";
+					item.get('items').forEach(function(model) {
+						panelTemplate += "<div class='panel-body'><a href='" + model.get('link') +"'>" + model.get('name') + "</a></div>";
+					});
+					panelTemplate += "</div>";
+				}
+			});
+			panelTemplate += "</div>";
+			this.template = _.template(panelTemplate);
+		}
+	});
+	return {
+		view: Panel,
+		collection: AccordionCollection,
+		model: Group
+	};
+});
---
+++ test/config.js
@@ -0,0 +1,30 @@
+requirejs.config({
+    paths : { 
+        jquery : 'libs/jquery',
+        jqueryui : 'libs/jqueryui',
+        jquerylayout : 'libs/jquerylayout',
+        jquerymigrate : 'libs/jquerymigrate',
+        underscore : 'libs/underscore',
+        backbone : 'libs/backbone',
+        handlebars : 'libs/handlebars',
+        marionette : 'libs/marionette',
+        wreqr: 'libs/wreqr',
+        babysitter: 'libs/babysitter',
+        highcharts: 'libs/highcharts',
+        text : 'libs/text',
+        chosen: 'libs/chosen',
+	bootstrap: 'libs/bootstrap/js/boostrap.js'
+
+	//accordionPanel: 
+    },
+    shim : {
+        'jqueryui' : { deps: ['jquery'], exports: 'jqueryui' },
+        'jquerymigrate': { deps: ['jquery'], exports: 'jquery'},
+        'jquerylayout': { deps: ['jquery', 'jqueryui', 'jquerymigrate'], exports: 'jquerylayout' },
+        'marionette': { deps : ['jquery', 'underscore', 'backbone'], exports: 'marionette' },
+        'highcharts': { deps: ['jquery'], exports: 'highcharts'},
+	'bootstrap': { deps: ['jquery'] }
+    }
+});
+
+require(['js/main']);
